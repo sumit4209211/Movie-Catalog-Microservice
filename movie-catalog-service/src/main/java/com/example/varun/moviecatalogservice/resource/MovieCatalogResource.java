2431f95963355e5f2b11b6fb65fa8c4fd2b431a6
varunu28 <varun.u28@gmail.com>
2019-05-17 18:54:04 -0700

    Updated Catalog service to receive ratings from ratings service

--- movie-catalog-service/src/main/java/com/example/varun/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/varun/moviecatalogservice/resource/MovieCatalogResource.java
@@ -3,13 +3,14 @@ package com.example.varun.moviecatalogservice.resource;
 import com.example.varun.moviecatalogservice.model.CatalogItem;
 import com.example.varun.moviecatalogservice.model.Movie;
 import com.example.varun.moviecatalogservice.model.Rating;
+import com.example.varun.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +21,26 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+
+        List<Rating> ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId,
+                UserRating.class).getUserRating();
 
         return ratings
                 .stream()
                 .map(rating -> {
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
                                                             Movie.class);
+//                    Movie movie = webClientBuilder.build()
+//                            .get()
+//                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                            .retrieve()
+//                            .bodyToMono(Movie.class)
+//                            .block();
                     return new CatalogItem(movie.getName(), "Desc", rating.getRating());
                 })
                 .collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/example/varun/ratingsdataservice/resource/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/varun/ratingsdataservice/resource/RatingsResource.java
@@ -1,9 +1,13 @@
 package com.example.varun.ratingsdataservice.resource;
 
 import com.example.varun.ratingsdataservice.model.Rating;
+import com.example.varun.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,18 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+       List<Rating> ratings = Arrays.asList(
+               new Rating("1234", 4),
+               new Rating("5678", 3)
+       );
+
+       UserRating userRating = new UserRating();
+       userRating.setUserRating(ratings);
+
+       return userRating;
     }
 }
